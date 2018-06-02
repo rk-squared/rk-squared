1928c965387803f0eeb052bc645ec2157419d544
Josh Kelley <joshkel@gmail.com>
2018-06-02 13:45:15 -0400

    Reorganize schemas into separate modules

---
+++ app/api/schemas/common.ts
@@ -0,0 +1,11 @@
+export type BoolAsString = '0' | '1';
+
+// A `/Content/lang/ww/compile` path
+export type ContentPath = string;
+
+// A `/dff/static/lang/ww/compile` path
+export type RelativeUrlPath = string;
+
+// Unix timestamp
+export type Timestamp = number;
+export type TimestampString = string;
--- app/api/schemas/index.ts
+++ app/api/schemas/index.ts
@@ -1,23 +1,10 @@
-import { ItemTypeName } from '../data/items';
+import { ItemTypeName } from '../../data/items';
+import { BoolAsString, ContentPath, RelativeUrlPath, Timestamp } from './common';
 
-type BoolAsString = '0' | '1';
-
-// A `/Content/lang/ww/compile` path
-type ContentPath = string;
-
-// A `/dff/static/lang/ww/compile` path
-type RelativeUrlPath = string;
-
-// Unix timestamp
-type Timestamp = number;
-type TimestampString = string;
-
-export enum LimitedRateServiceType {
-  HalfPriceDungeonLogic = 14,
-  HalfPriceDungeonBanner = 15,
-}
+export { Main } from './main';
 
 export enum RecordMateriaEffectType {
+  // noinspection JSUnusedGlobalSymbols
   FullAtb = 1,
   StatBoost = 3,
   StartBuff = 4,
@@ -48,6 +35,7 @@ export enum RecordMateriaEffectType {
 }
 
 export enum RewardType {
+  // noinspection JSUnusedGlobalSymbols
   Completion = '1',
   FirstTime = '2',
   Mastery = '3',
@@ -189,114 +177,6 @@ interface Equipment {
     type: string;
     attribute_id: string;
   }>;
-}
-
-// Data extracted from the main http://ffrk.denagames.com/dff/ startup request
-export interface Main {
-  appInitData: {
-    user: {
-      stamina_piece: number;
-      soul_piece: number;
-      residual_stamina_piece: number;
-      start_time_of_today: number;
-      stamina_recovery_time: number;
-      max_followee_num: number;
-      dungeon_id: number;
-      id: number;
-      release_id: number;
-      stamina_info: {
-        additional_stamina: number;
-        current_max_stamina: number;
-        prev_max_stamina: number;
-        got_stamina_piece: number;
-      };
-      func_tutorial_flags: {
-        [s: string]: boolean;
-      }
-      name: string;
-      has_all_clear_ver_to_show: false;
-      stamina: number;
-      can_review: true;
-      tutorial_step: number;
-      followee_num: number;
-      stamina_recovery_remaining_time: number;
-      gil: number;
-      is_update_last_logined_at: false;
-      last_logined_at: number;
-      max_follower_num: number;
-      max_stamina: number;
-    }
-
-    user_stamina_recovery_agents: Array<{
-      num: number;
-      stamina_recovery_agent_id: number;  // 94100001 for stamina potions
-    }>;
-
-    worlds: Array<{
-      has_brave_series_buddies: boolean;
-      closed_at: Timestamp;
-      bgm: string;
-      dungeon_status_summary: Array<{
-        clear_count: number;
-        master_count: number;
-      }>;
-      door_image_path: RelativeUrlPath;
-      dungeon_term_list?: Array<{
-        closed_at: TimestampString;
-        id: string;
-        type: string;
-        opened: TimestampString;
-      }>;
-      series_formal_name: string;
-      id: number;
-      name: string;
-      has_new_dungeon: boolean;
-      series_id: number;
-      opened_at: number;
-      kept_out_at: Timestamp;
-      is_unlocked: boolean;  // May be false for events that are not yet entered
-      image_path: RelativeUrlPath;
-      type: number;
-      banner_message: string;
-    }>;
-
-    events: Array<{
-      world_id: number;
-      battle_list_bg_type: number;
-      // challenge (incl. GL Torment), beast, extreme (GL Nightmare), suppress (MO), wday (Power Up Dungeons),
-      // rotation (mote dungeons)
-      type_name: string;
-      has_intro_movie: boolean;
-      ex_opened_at: Timestamp;
-      order_weight: number;
-      image_path: RelativeUrlPath;
-      type: number;
-      id: number;
-      // e.g., "nightmare_dungeon" (Torment), "beast_element_dungeon", "crystal_tower", "ff4_reopen_ww201804"
-      tag: string;
-      background_image_path: RelativeUrlPath;
-    }>;
-
-    limited_rate_services?: Array<{
-      // For a 1/2 stamina banner image, this is wait_sec_to_close 258570,
-      // img_path, type 15, wait_sec_to_open 0.
-      //
-      // For a 1/2 stamina internal data, this is rate "0.5", dungeon_id_map,
-      // play_mode 0, is_host 0, wait_sec_to_close 258570, type 14,
-      // wait_sec_to_open 0.
-      play_mode?: number;
-      is_host?: number;
-      rate?: string;
-      dungeon_id_map?: { [id: string]: number };
-      img_path?: RelativeUrlPath;
-      type: LimitedRateServiceType;
-      wait_sec_to_open: number;
-      wait_sec_to_close: number;
-    }>;
-  };
-  textMaster: {
-    [s: string]: string;
-  };
 }
 
 export interface DropItem {
