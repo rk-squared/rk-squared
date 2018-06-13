efeb1b2f1cfa7486f51486490ac689074b5df0a5
Josh Kelley <joshkel@gmail.com>
2018-06-12 21:00:47 -0400

    Add filtering for record materia display
    Clean up grid styling and formatting.

--- app/components/RecordMateriaGrid.tsx
+++ app/components/RecordMateriaGrid.tsx
@@ -10,6 +10,10 @@ interface Props {
   recordMateria: RecordMateriaDetail[];
 }
 
+interface State {
+  filter: string | undefined;
+}
+
 type Comparator = ColDef['comparator'];
 function compareByNumberField(fieldName: string): Comparator {
   return (valueA, valueB, nodeA, nodeB) => {
@@ -18,7 +22,7 @@ function compareByNumberField(fieldName: string): Comparator {
   };
 }
 
-export class RecordMateriaGrid extends React.Component<Props> {
+export class RecordMateriaGrid extends React.Component<Props, State> {
   columnDefs: ColDef[];
 
   constructor(props: Props) {
@@ -48,22 +52,34 @@ export class RecordMateriaGrid extends React.Component<Props> {
         comparator: compareByNumberField('status'),
       },
     ];
+    this.state = {
+      filter: undefined
+    };
+  }
+
+  handleFilter = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({filter: e.currentTarget.value});
   }
 
   render() {
     const { recordMateria } = this.props;
     return (
-      <div
-        className="ag-theme-balham"
-        style={{
-          height: '500px',
-          width: '100%' }}
-      >
+      <div style={{ height: '500px', width: '100%' }} className="ag-theme-balham">
+        <div className="form-group">
+          <input
+            className="form-control form-control-sm"
+            placeholder="Search..."
+            value={this.state.filter}
+            onChange={this.handleFilter}
+          />
+        </div>
         <AgGridReact
           enableSorting={true}
           enableColResize={true}
           columnDefs={this.columnDefs}
-          rowData={recordMateria}>
+          rowData={recordMateria}
+          quickFilterText={this.state.filter}
+        >
         </AgGridReact>
       </div>
     );
