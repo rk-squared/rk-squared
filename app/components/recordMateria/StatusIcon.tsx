c2e46811a85a22215f91c4be66c289682bfe5f92
Josh Kelley <joshkel@gmail.com>
2018-06-19 20:40:49 -0400

    Color-code status icons

--- app/components/recordMateria/StatusIcon.tsx
+++ app/components/recordMateria/StatusIcon.tsx
@@ -3,24 +3,26 @@ const { default: FontAwesomeIcon } = require('@fortawesome/react-fontawesome');
 
 import { RecordMateriaStatus } from '../../actions/recordMateria';
 
+const styles = require('./StatusIcon.scss');
+
 export class StatusIcon extends React.Component<{status: RecordMateriaStatus}> {
   render() {
     const { status } = this.props;
     switch (status) {
       case RecordMateriaStatus.Unobtained:
-        return <FontAwesomeIcon icon="question"/>;
+        return <FontAwesomeIcon classNames={styles.unobtained} icon="question"/>;
       case RecordMateriaStatus.LockedLowLevel:
-        return <FontAwesomeIcon icon="arrow-down"/>;
+        return <FontAwesomeIcon className={styles.lockedLowLevel} icon="arrow-down"/>;
       case RecordMateriaStatus.LockedMissingPrereq:
-        return <FontAwesomeIcon icon="ellipsis-h"/>;
+        return <FontAwesomeIcon className={styles.lockedMissingPrereq} icon="ellipsis-h"/>;
       case RecordMateriaStatus.Unlocked:
-        return <FontAwesomeIcon icon="lock-open"/>;
+        return <FontAwesomeIcon className={styles.unlocked} icon="lock-open"/>;
       case RecordMateriaStatus.Collected:
-        return <FontAwesomeIcon icon="check"/>;
+        return <FontAwesomeIcon className={styles.collected} icon="check"/>;
       case RecordMateriaStatus.Favorite:
-        return <FontAwesomeIcon icon="star"/>;
+        return <FontAwesomeIcon className={styles.favorite} icon="star"/>;
       case RecordMateriaStatus.Vault:
-        return <FontAwesomeIcon icon="archive"/>;
+        return <FontAwesomeIcon className={styles.vault} icon="archive"/>;
     }
   }
 }
