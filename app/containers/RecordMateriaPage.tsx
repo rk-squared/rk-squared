d187dac362c1e27aacca6abe49a4893673b39d6f
Josh Kelley <joshkel@gmail.com>
2018-06-23 13:59:19 -0400

    Routing

--- app/containers/RecordMateriaPage.tsx
+++ app/containers/RecordMateriaPage.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
 
 import { getRecordMateriaDetail, RecordMateriaDetail } from '../actions/recordMateria';
-import { RecordMateriaGrid } from '../components/recordMateria/RecordMateriaGrid';
 import { IState } from '../reducers';
 import { Page } from './Page';
-import { StatBuffs } from '../components/recordMateria/StatBuffs';
+import { RecordMateriaRoutes } from './RecordMateriaRoutes';
 
 interface Props {
   recordMateria: RecordMateriaDetail[];
 }
 
-export class RecordMateriaPage extends React.Component<Props> {
+export class RecordMateriaPage extends React.Component<Props & RouteComponentProps<Props>> {
   render() {
-    const { recordMateria } = this.props;
+    const { recordMateria, match, location, history} = this.props;
     return (
       <Page title="Record Materia">
         {recordMateria.length === 0
@@ -21,7 +21,7 @@ export class RecordMateriaPage extends React.Component<Props> {
               <p>Record materia information has not been loaded.</p>
               <p>Within FFRK, please go under the Annex, under the Library, and choose Record Materia.</p>
             </div>
-          : <div><StatBuffs recordMateria={recordMateria}/><RecordMateriaGrid recordMateria={recordMateria}/></div>
+          : <RecordMateriaRoutes recordMateria={recordMateria} match={match} location={location} history={history}/>
         }
       </Page>
     );
---
+++ app/containers/RecordMateriaRoutes.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Route, RouteComponentProps } from 'react-router';
+import { NavLink } from 'react-router-dom';
+
+import { RecordMateriaDetail } from '../actions/recordMateria';
+import { RecordMateriaGrid } from '../components/recordMateria/RecordMateriaGrid';
+import { StatBuffs } from '../components/recordMateria/StatBuffs';
+
+const styles = require('./RecordMateriaRoutes.scss');
+
+interface Props {
+  recordMateria: RecordMateriaDetail[];
+}
+
+export class RecordMateriaRoutes extends React.Component<Props & RouteComponentProps<Props>> {
+  renderAll = () => <RecordMateriaGrid recordMateria={this.props.recordMateria}/>;
+  renderStatBuffs = () => <StatBuffs recordMateria={this.props.recordMateria}/>;
+
+  render() {
+    const { match } = this.props;
+
+    const items: Array<[string, string, () => React.ReactNode]> = [
+      ['All', '', this.renderAll],
+      ['Stat Buffs', '/statBuffs', this.renderStatBuffs],
+    ];
+
+    return (
+      <div className={styles.component}>
+        <ul className="nav nav-tabs">
+          {items.map(([text, subUrl, render], index) =>
+            <li className="nav-item" key={index}>
+              <NavLink exact className="nav-link" activeClassName="active" to={match.url + subUrl}>{text}</NavLink>
+            </li>
+          )}
+        </ul>
+
+        {items.map(([text, subUrl, render], index) =>
+          <Route exact key={index} path={match.url + subUrl} render={render}/>
+        )}
+      </div>
+    );
+  }
+}
