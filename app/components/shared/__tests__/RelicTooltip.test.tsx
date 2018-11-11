c5e37dd3bd52e56605a183584620ca7c12a8c58a
Josh Kelley <joshkel@gmail.com>
2018-11-11 17:21:17 -0500

    Add unit tests; refactor battle.ts to make it more testable

---
+++ app/components/shared/__tests__/RelicTooltip.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { RelicTooltip } from '../RelicTooltip';
+
+describe('RelicTooltip', () => {
+  it('renders an accessory tooltip', () => {
+    const tree = renderer
+      .create(<RelicTooltip id="test" relicId={23080334}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders nothing when given an unknown relic', () => {
+    const tree = renderer
+      .create(<RelicTooltip id="test" relicId={1}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
