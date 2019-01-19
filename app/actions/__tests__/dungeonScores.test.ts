933471665d6068b23e833a0bc74b8780cd0785e2
Josh Kelley <joshkel@gmail.com>
2019-01-19 10:18:28 -0500

    Tests and new items

--- app/actions/__tests__/dungeonScores.test.ts
+++ app/actions/__tests__/dungeonScores.test.ts
@@ -13,7 +13,7 @@ describe('actions/dungeonScores', () => {
       expect(formatScore(score)).toEqual('71%');
     });
 
-    it('shows time for completed Tormets', () => {
+    it('shows time for completed Torments', () => {
       const score = {
         type: DungeonScoreType.PERCENT_HP_OR_CLEAR_TIME,
         maxHp: 1000000,
@@ -22,6 +22,24 @@ describe('actions/dungeonScores', () => {
         won: true,
       };
       expect(formatScore(score)).toEqual('22.68');
+    });
+
+    it('handles damage races like Bomb Brigade', () => {
+      const score = {
+        type: DungeonScoreType.TOTAL_DAMAGE,
+        totalDamage: 460895,
+        won: true,
+      };
+      expect(formatScore(score)).toEqual('460,895 HP');
+    });
+
+    it('handles slow magicite wins', () => {
+      const score = {
+        type: DungeonScoreType.CLEAR_TIME,
+        time: 60 * 1000 + 2345,
+        won: true,
+      };
+      expect(formatScore(score)).toEqual('1:02.34');
     });
   });
 });
