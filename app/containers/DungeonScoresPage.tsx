0a43ce0b06f0521cba4f025ecf975d82a5b0fbd4
Josh Kelley <joshkel@gmail.com>
2019-01-19 12:04:28 -0500

    Add a tip about loading dungeons

--- app/containers/DungeonScoresPage.tsx
+++ app/containers/DungeonScoresPage.tsx
@@ -3,12 +3,18 @@ import * as React from 'react';
 import TormentGrid from '../components/dungeonScores/TormentGrid';
 import { Page } from './Page';
 
+const styles = require('./DungeonScoresPage.scss');
+
 export default class DungeonScoresPage extends React.Component {
   render() {
     return (
       <Page title="Dungeon Scores">
         <h4>Torments</h4>
         <TormentGrid />
+        <div className={`alert alert-secondary ${styles.howToTip}`}>
+          <strong>Tip:</strong> You may need to enter a dungeon in order to get complete time and
+          percent complete information.
+        </div>
       </Page>
     );
   }
