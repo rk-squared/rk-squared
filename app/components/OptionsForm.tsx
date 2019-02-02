6affafca5ac79a668325898c0950b1b88c615b82
Josh Kelley <joshkel@gmail.com>
2019-02-02 10:40:06 -0500

    Add icon attribution
    Run Prettier.

--- app/components/OptionsForm.tsx
+++ app/components/OptionsForm.tsx
@@ -3,14 +3,15 @@ import { connect } from 'react-redux';
 
 import { Options, setOption as setOptionAction } from '../actions/options';
 import { IState } from '../reducers';
-import BrowserLink from './BrowserLink';
+import { BrowserLink } from './BrowserLink';
 
 const styles = require('./OptionsForm.scss');
 
-const ENLIR = 'https://docs.google.com/spreadsheets/d/16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ/edit';
-const ENLIR_HELP = 'Open Enlir\'s Google Docs spreadsheet in your browser.';
+const ENLIR =
+  'https://docs.google.com/spreadsheets/d/16K1Zryyxrh7vdKVF1f7eRrUAOC5wuzvC3q2gFLch6LQ/edit';
+const ENLIR_HELP = "Open Enlir's Google Docs spreadsheet in your browser.";
 const MISTER_P = 'http://happypluto.com/~misterp/r/ffrk.pdf';
-const MISTER_P_HELP = 'Open MisterP\'s FFRK PDF in your browser.';
+const MISTER_P_HELP = "Open MisterP's FFRK PDF in your browser.";
 
 interface Props {
   options: Options;
@@ -18,27 +19,34 @@ interface Props {
   setOption: (newOptions: Options) => any;
 }
 
-const Checkbox = (
-  { id, children, options, setOption }:
-    { id: string, children: any, options: Options, setOption: (o: Options) => void }
-) => (
-    <div className="form-check">
-      <input
-        className="form-check-input"
-        id={id} name={id} type="checkbox"
-        checked={(options as any)[id]}
-        onChange={() => setOption({[id]: !(options as any)[id]})}
-      />
-      <label className="form-check-label" htmlFor={id}>
-        {children}
-      </label>
-    </div>
+const Checkbox = ({
+  id,
+  children,
+  options,
+  setOption,
+}: {
+  id: string;
+  children: any;
+  options: Options;
+  setOption: (o: Options) => void;
+}) => (
+  <div className="form-check">
+    <input
+      className="form-check-input"
+      id={id}
+      name={id}
+      type="checkbox"
+      checked={(options as any)[id]}
+      onChange={() => setOption({ [id]: !(options as any)[id] })}
+    />
+    <label className="form-check-label" htmlFor={id}>
+      {children}
+    </label>
+  </div>
 );
 
-const HelpText = ({children}: {children: any}) => (
-  <small className="form-text text-muted">
-    {children}
-  </small>
+const HelpText = ({ children }: { children: any }) => (
+  <small className="form-text text-muted">{children}</small>
 );
 
 export class OptionsForm extends React.Component<Props> {
@@ -47,43 +55,53 @@ export class OptionsForm extends React.Component<Props> {
     return (
       <div className={styles.component}>
         <div className="form-group">
-          <Checkbox id="alwaysShowTimer" {...{options, setOption}}>
+          <Checkbox id="alwaysShowTimer" {...{ options, setOption }}>
             Always show timer
           </Checkbox>
           <HelpText>
-            Besides self-imposed speedrun challenges, this can be useful for tracking
-            when buffs and debuffs might expire.
-            Check <BrowserLink href={ENLIR} title={ENLIR_HELP}>Enlir</BrowserLink> or{' '}
-            <BrowserLink href={MISTER_P} title={MISTER_P_HELP}>MisterP</BrowserLink> for
-            details on buff durations.
+            Besides self-imposed speedrun challenges, this can be useful for tracking when buffs and
+            debuffs might expire. Check{' '}
+            <BrowserLink href={ENLIR} title={ENLIR_HELP}>
+              Enlir
+            </BrowserLink>{' '}
+            or{' '}
+            <BrowserLink href={MISTER_P} title={MISTER_P_HELP}>
+              MisterP
+            </BrowserLink>{' '}
+            for details on buff durations.
           </HelpText>
         </div>
 
         <div className="form-group">
-          <Checkbox id="staticBattleBackground" {...{options, setOption}}>
+          <Checkbox id="staticBattleBackground" {...{ options, setOption }}>
             Show static battle backgrounds
           </Checkbox>
           <HelpText>
             <p>
-              Replaces all battle backgrounds with the Nightmare's plain, dark caverns.
-              This may help performance on older phones or tablets.
+              Replaces all battle backgrounds with the Nightmare's plain, dark caverns. This may
+              help performance on older phones or tablets.
             </p>
             <p>
-              You may also want to enable &ldquo;Simplified Display&rdquo; in the in-game
-              Config menu to reduce animations and effects on game menus.
+              You may also want to enable &ldquo;Simplified Display&rdquo; in the in-game Config
+              menu to reduce animations and effects on game menus.
             </p>
           </HelpText>
         </div>
 
         <div className="form-group">
-          <Checkbox id="saveTrafficCaptures" {...{options, setOption}}>
+          <Checkbox id="saveTrafficCaptures" {...{ options, setOption }}>
             Save captured game traffic
           </Checkbox>
           <HelpText>
             <p>
               Save captured FFRK network traffic
-              {capturePath && <span> under <code>{capturePath}</code></span>}.
-              This can be helpful for testing and troubleshooting but is otherwise not needed.
+              {capturePath && (
+                <span>
+                  {' '}
+                  under <code>{capturePath}</code>
+                </span>
+              )}
+              . This can be helpful for testing and troubleshooting but is otherwise not needed.
             </p>
           </HelpText>
         </div>
@@ -95,6 +113,6 @@ export class OptionsForm extends React.Component<Props> {
 export default connect(
   ({ options, proxy }: IState) => ({ options, capturePath: proxy.capturePath }),
   dispatch => ({
-    setOption: (newOptions: Options) => dispatch(setOptionAction(newOptions))
-  })
+    setOption: (newOptions: Options) => dispatch(setOptionAction(newOptions)),
+  }),
 )(OptionsForm);
--- app/containers/DungeonsPage.tsx
+++ app/containers/DungeonsPage.tsx
@@ -8,6 +8,7 @@ import { World } from '../actions/worlds';
 import { IState } from '../reducers';
 import { hasSessionState } from '../reducers/session';
 
+import { BrowserLink } from '../components/BrowserLink';
 import { ProgressBar } from '../components/common/ProgressBar';
 import { DungeonsList } from '../components/dungeons/DungeonsList';
 import ItemTypeChecklist from '../components/dungeons/ItemTypeChecklist';
@@ -34,45 +35,57 @@ export class DungeonsPage extends React.Component<Props> {
 
   render() {
     const { worlds, missingWorlds, progress, hasSession } = this.props;
-    const missingPrompt = missingWorlds.length === 1 ? '1 realm or event' : `${missingWorlds.length} realms and events`;
+    const missingPrompt =
+      missingWorlds.length === 1 ? '1 realm or event' : `${missingWorlds.length} realms and events`;
     return (
       <Page title="Dungeon Tracker">
-        {missingWorlds.length !== 0 && hasSession && !progress &&
+        {missingWorlds.length !== 0 && hasSession && !progress && (
           <p>
             Dungeons for {missingPrompt} have not been loaded.{' '}
-            <a href="#" onClick={this.handleLoad}>Load now?</a>
+            <a href="#" onClick={this.handleLoad}>
+              Load now?
+            </a>
           </p>
-        }
+        )}
 
-        {progress &&
+        {progress && (
           <div className="mb-2">
             Loading dungeons for {progress.current + 1} of {progress.max}&hellip;
-            <ProgressBar progress={progress}/>
+            <ProgressBar progress={progress} />
           </div>
-        }
+        )}
 
-        {worlds == null
-          ? <p>No dungeons have been loaded. Please check your proxy settings and restart FFRK.</p>
-          : <div className="row">
-              <DungeonsList className="col-sm-9" worlds={worlds}/>
-              <div className={`col-sm-3 ${styles.right}`}>
-                <ItemTypeChecklist title="Rewards"/>
-              </div>
+        {worlds == null ? (
+          <p>No dungeons have been loaded. Please check your proxy settings and restart FFRK.</p>
+        ) : (
+          <div className="row">
+            <div className="col-sm-9">
+              <DungeonsList worlds={worlds} />
+              <p className="text-muted text-right mb-0">
+                <small>
+                  Icons by{' '}
+                  <BrowserLink href="https://game-icons.net/" className="text-muted">
+                    game-icons.net
+                  </BrowserLink>
+                </small>
+              </p>
             </div>
-        }
+            <div className={`col-sm-3 ${styles.right}`}>
+              <ItemTypeChecklist title="Rewards" />
+            </div>
+          </div>
+        )}
       </Page>
     );
   }
 }
 
-export default connect(
-  (state: IState) => ({
-    worlds: state.worlds.worlds,
-    missingWorlds:  // FIXME: Use reselect (I think)
-      Object.keys(state.worlds.worlds || {})
-        .map(i => +i)
-        .filter(i => (state.worlds.worlds || {})[i].isUnlocked && !state.dungeons.byWorld[i]),
-    progress: state.progress.dungeons,
-    hasSession: hasSessionState(state.session),
-  })
-)(DungeonsPage);
+export default connect((state: IState) => ({
+  worlds: state.worlds.worlds,
+  // FIXME: Use reselect (I think)
+  missingWorlds: Object.keys(state.worlds.worlds || {})
+    .map(i => +i)
+    .filter(i => (state.worlds.worlds || {})[i].isUnlocked && !state.dungeons.byWorld[i]),
+  progress: state.progress.dungeons,
+  hasSession: hasSessionState(state.session),
+}))(DungeonsPage);
