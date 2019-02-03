235cb97f4cc1d3411d374e4183e0d421419dae91
Josh Kelley <joshkel@gmail.com>
2019-02-02 22:22:20 -0500

    Add donation URL

--- app/components/DonationInfo.tsx
+++ app/components/DonationInfo.tsx
@@ -4,12 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { BrowserLink } from './BrowserLink';
 
+const URL = 'http://ko-fi.com/rksquared';
+
 export class DonationInfo extends React.Component {
   render() {
     return (
       <div className="alert alert-info">
         Like RK&sup2;? Want to help support its development?{' '}
-        <BrowserLink href="#" className="alert-link">
+        <BrowserLink href={URL} className="alert-link">
           Buy me a coffee!
         </BrowserLink>{' '}
         <FontAwesomeIcon icon="coffee-togo" />
--- app/containers/OptionsPage.tsx
+++ app/containers/OptionsPage.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+import { DonationInfo } from '../components/DonationInfo';
 import OptionsForm from '../components/OptionsForm';
 import { Page } from './Page';
 
 export default class OptionsPage extends React.Component<RouteComponentProps<any>> {
+  renderFooter = () => <DonationInfo />;
+
   render() {
     return (
-      <Page title="Options">
+      <Page title="Options" footer={this.renderFooter}>
         <p>
-          Modify FFRK to improve your gaming experience.  Unless otherwise
-          noted, all options take effect as soon as you enter the appropriate
-          screen.
+          Modify FFRK to improve your gaming experience. Unless otherwise noted, all options take
+          effect as soon as you enter the appropriate screen.
         </p>
-        <OptionsForm/>
+        <OptionsForm />
       </Page>
     );
   }
