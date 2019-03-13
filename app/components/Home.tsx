89df01a4fb748c5971e041d3fd00dac7a021de57
Josh Kelley <joshkel@gmail.com>
2019-03-12 21:59:36 -0400

    Partially functioning
    Merge a bunch of configs from electron-react-boilerplate.  Update a
    bunch of packages.  Dev. seems to work, although hot reload is broken as
    before.  Packaging fails.

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -9,22 +9,20 @@ export class Home extends React.Component {
     return (
       <div>
         <p>
-          RK&sup2; runs as a <em>proxy</em>, which means that you need to configure your
-          phone or tablet to send its Internet traffic through RK&sup2;.
+          RK&sup2; runs as a <em>proxy</em>, which means that you need to configure your phone or
+          tablet to send its Internet traffic through RK&sup2;.
         </p>
 
         <p>
-          Depending on which mobile device you're using, you may also need to install
-          a <em>certificate</em> to let RK&sup2; impersonate the FFRK servers.
+          Depending on which mobile device you're using, you may also need to install a{' '}
+          <em>certificate</em> to let RK&sup2; impersonate the FFRK servers.
         </p>
 
-        <p>
-          Once this is done, it can automatically track and help you manage your game progress.
-        </p>
+        <p>Once this is done, it can automatically track and help you manage your game progress.</p>
 
-        <ProxyStatusDisplay/>
-        <AndroidInstructions/>
-        <IosInstructions/>
+        <ProxyStatusDisplay />
+        <AndroidInstructions />
+        <IosInstructions />
       </div>
     );
   }
