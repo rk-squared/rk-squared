ae0a359173c4a578d0fae992231e62c647d363e1
Josh Kelley <joshkel@gmail.com>
2019-04-23 23:07:57 -0400

    Finish initial implementation

--- app/components/relicDraws/RelicChances.tsx
+++ app/components/relicDraws/RelicChances.tsx
@@ -10,6 +10,7 @@ import { enlir } from '../../data/enlir';
 import { chanceOfDesiredDrawProp5 } from '../../data/probabilities';
 import { IState } from '../../reducers';
 import { RelicDrawBannerDetails } from '../../selectors/relicDraws';
+import { pluralize } from '../../utils/textUtils';
 
 const styles = require('./RelicChances.scss');
 
@@ -32,35 +33,73 @@ function getRelicChanceDetails(
   }
 
   const rareChancePerRelic = probabilities.byRarity[5] + probabilities.byRarity[6];
-  const desiredChancePerRelic = want
-    ? _.sum(_.map(probabilities.byRelic, (chance: number, id: number) => (want[id] ? chance : 0)))
-    : 0;
+  let desiredCountAndChance: [number, number];
+  if (want) {
+    desiredCountAndChance = _.map(
+      probabilities.byRelic,
+      (chance: number, id: string): [number, number] => (want[+id] ? [1, chance] : [0, 0]),
+    ).reduce(([a, b], [c, d]) => [a + c, b + d], [0, 0]);
+  } else {
+    desiredCountAndChance = [0, 0];
+  }
 
   const sixStarCount = _.keys(probabilities.byRelic)
     .map(i => enlir.relics[i])
     .filter(i => !!i && i.rarity >= 6).length;
 
+  // FIXME: Get this from banner details
+  const drawCount = 11;
+
   const totalDetails = chanceOfDesiredDrawProp5(
-    banner.totalCount,
+    drawCount,
     rareChancePerRelic / 100,
     rareChancePerRelic / 100,
   );
   const desiredDetails = chanceOfDesiredDrawProp5(
-    banner.totalCount,
+    drawCount,
     rareChancePerRelic / 100,
-    desiredChancePerRelic / 100,
+    desiredCountAndChance[1] / 100,
   );
 
   return {
     rareChancePerRelic,
-    desiredChancePerRelic,
+    desiredCount: desiredCountAndChance[0],
+    desiredChancePerRelic: desiredCountAndChance[1],
     sixStarCount,
     expectedValue: totalDetails.expectedValue,
     desiredChance: desiredDetails.desiredChance,
   };
 }
 
+const chanceAfterNDraws = (chance: number, n: number) => (1 - (1 - chance) ** n) * 100;
+
 export class RelicChances extends React.PureComponent<Props> {
+  renderWant(count: number, chance: number) {
+    if (!count) {
+      return (
+        <p className="card-text">
+          Select one or more relics you want from this banner to see the chances of pulling what you
+          want.
+        </p>
+      );
+    }
+    return (
+      <div>
+        <p className="card-text">
+          Chance of getting {count === 1 ? '1 selected relic ' : `≥1 of ${count} selected relics `}
+          after&hellip;
+        </p>
+        <div className={styles.want}>
+          {_.times(6, i => (
+            <div key={i} className={styles.wantItem}>
+              <span className={styles.wantCount}>{i + 1 + ' ' + pluralize(i + 1, 'pull')}</span>
+              {chanceAfterNDraws(chance, i + 1).toFixed(2)}%
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
   render() {
     const { banner, probabilities, want, isAnonymous, className } = this.props;
     if (!banner.totalCount) {
@@ -77,23 +116,28 @@ export class RelicChances extends React.PureComponent<Props> {
         <div className="card-img-top bg-success text-white">
           <FontAwesomeIcon icon={['fal', 'dice-d20']} size="2x" />
         </div>
-        <div className="card-body">
-          <p className="card-text">
-            {details.rareChancePerRelic.toFixed(2)}% chance of 5★ or better
-          </p>
-          <p className="card-text">
-            (ave. {details.expectedValue.toFixed(2)} 5★ or better per 11× pull)
-          </p>
-          <p className="card-text">
-            {details.sixStarCount} / {banner.totalCount} 6★ relics
-          </p>
-          {!isAnonymous && banner.dupeCount != null && (
+        <div className="card-body row">
+          <div className="col-sm-6">
             <p className="card-text">
-              <span className={styles.dupe}>
-                {banner.dupeCount} / {banner.totalCount} dupes
-              </span>
+              {details.rareChancePerRelic.toFixed(2)}% chance of 5★ or better
             </p>
-          )}
+            <p className="card-text">
+              (ave. {details.expectedValue.toFixed(2)} 5★ or better per 11× pull)
+            </p>
+            <p className="card-text">
+              {details.sixStarCount} / {banner.totalCount} 6★ relics
+            </p>
+            {!isAnonymous && banner.dupeCount != null && (
+              <p className="card-text">
+                <span className={styles.dupe}>
+                  {banner.dupeCount} / {banner.totalCount} dupes
+                </span>
+              </p>
+            )}
+          </div>
+          <div className="col-sm-6">
+            {this.renderWant(details.desiredCount, details.desiredChance)}
+          </div>
         </div>
       </div>
     );
