f21f87c8a8e0b0c6e084428c0cf211163c78835e
Josh Kelley <joshkel@gmail.com>
2019-08-30 22:48:22 -0400

    Refactor out element colors; start implementing orb details

---
+++ app/data/__tests__/orbDetails.test.ts
@@ -0,0 +1,33 @@
+import { getOrbCosts, parseOrb } from '../orbDetails';
+
+import { enlir } from '../enlir';
+
+describe('orbDetails', () => {
+  describe('parseOrb', () => {
+    it('parses orbs', () => {
+      expect(parseOrb('Minor Non-Elemental')).toEqual(['NE', 1]);
+    });
+  });
+
+  describe('getOrbCosts', () => {
+    it('gets orb costs', () => {
+      expect(getOrbCosts(enlir.abilitiesByName['Chain Firaja'])).toEqual([
+        {
+          orbType: 'Black',
+          cost: '10-',
+          rarity: 6,
+        },
+        {
+          orbType: 'NE',
+          cost: '6',
+          rarity: 6,
+        },
+        {
+          orbType: 'Fire',
+          cost: '6',
+          rarity: 6,
+        },
+      ]);
+    });
+  });
+});
---
+++ app/data/orbDetails.ts
@@ -0,0 +1,81 @@
+import * as _ from 'lodash';
+
+import { EnlirAbility } from './enlir';
+
+export type OrbType =
+  | 'Fire'
+  | 'Ice'
+  | 'Lightning'
+  | 'Earth'
+  | 'Wind'
+  | 'Holy'
+  | 'Dark'
+  | 'NE'
+  | 'Power'
+  | 'Black'
+  | 'White'
+  | 'Summon';
+
+export const orbCosts = {
+  '1': [1, 2, 3, 4, 5],
+  '2': [2, 3, 6, 12, 18],
+  '2+': [2, 5, 10, 20, 30],
+  '3<': [3, 6, 9, 12, 15],
+  '3--': [3, 5, 10, 15, 25],
+  '3-': [3, 5, 10, 20, 30],
+  '3': [3, 6, 12, 18, 30],
+  '3+': [3, 8, 15, 30, 45],
+  '5': [5, 10, 20, 30, 50],
+  '6+': [6, 12, 24, 36, 60],
+  '6': [6, 12, 18, 30, 60],
+  '6-': [6, 10, 20, 40, 60],
+  '8': [8, 15, 30, 45, 75],
+  '10': [10, 20, 40, 60, 100],
+  '10-': [10, 20, 30, 50, 100],
+  '15': [15, 30, 45, 75, 150],
+};
+export type CostType = keyof typeof orbCosts;
+
+const orbCostLookup = _.invert(_.mapValues(orbCosts, _.sum));
+
+const levelToRarity: { [s: string]: number } = {
+  Minor: 1,
+  Lesser: 2,
+  Greater: 4,
+  Major: 5,
+  Crystal: 6,
+};
+const defaultRarity = 3;
+
+export function parseOrb(orbName: string): [OrbType, number] {
+  const m = orbName.match(/^(Minor |Lesser ||Greater |Major )?(.*?)( Orb| Crystal)?$/);
+  if (!m) {
+    throw new Error(`Failed to parse ${orbName}`);
+  }
+  const [, level, typeName, orbOrCrystal] = m;
+
+  const rarity =
+    levelToRarity[(orbOrCrystal || '').trim()] || levelToRarity[level.trim()] || defaultRarity;
+  const orbType =
+    typeName === 'Non-Elemental'
+      ? 'NE'
+      : typeName.match(/^Summon/)
+      ? 'Summon'
+      : (typeName as OrbType);
+  return [orbType, rarity];
+}
+
+export function getOrbCosts(ability: EnlirAbility) {
+  return _.toPairs(ability.orbs)
+    .filter(([i]) => i !== 'Ability Record')
+    .sort(([orbName, costs]) => -costs[0])
+    .map(([orbName, costs]) => {
+      const [orbType, rarity] = parseOrb(orbName);
+      const cost = orbCostLookup[_.sum(costs)] as CostType;
+      return {
+        orbType,
+        rarity,
+        cost,
+      };
+    });
+}
