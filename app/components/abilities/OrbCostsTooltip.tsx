d242fd5a922262944cc7f1e28183e07f090c4dd1
Josh Kelley <joshkel@gmail.com>
2019-09-24 23:19:08 -0400

    Better display of Record Board costs

--- app/components/abilities/OrbCostsTooltip.tsx
+++ app/components/abilities/OrbCostsTooltip.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import * as ReactTooltip from 'react-tooltip';
 
+import * as _ from 'lodash';
+
 import { LangType } from '../../api/apiUrls';
 import { LangContext } from '../../contexts/LangContext';
 import { enlir, MAX_ABILITY_RANK } from '../../data/enlir';
 import { itemsById, ItemType } from '../../data/items';
-import { getOrbCosts, orbCosts } from '../../data/orbDetails';
+import { getOrbCosts, OrbCost, orbCosts } from '../../data/orbDetails';
 import * as urls from '../../data/urls';
 import { runningTotal } from '../../utils/typeUtils';
 
@@ -22,6 +24,15 @@ export class OrbCostsTooltip extends React.Component<Props> {
   static contextType = LangContext;
   context!: React.ContextType<typeof LangContext>;
 
+  getStartingRank(costs: OrbCost[]): number {
+    if (_.every(costs.map(i => orbCosts[i.cost] && orbCosts[i.cost][0] === 0))) {
+      // If rank 1 has no cost, then start at rank 2.
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+
   getContent = (abilityId: string) => {
     if (!abilityId) {
       return null;
@@ -29,16 +40,18 @@ export class OrbCostsTooltip extends React.Component<Props> {
     const ability = enlir.abilities[abilityId];
     const lang = ability.gl ? (this.context as LangType) : LangType.Jp;
     const costs = getOrbCosts(ability);
+    const startingRank = this.getStartingRank(costs);
+    const rankCount = MAX_ABILITY_RANK - startingRank + 1;
     return (
       <table className={classNames(styles.component, 'table table-dark table-bordered')}>
         <thead>
           <tr>
             <th>Orb</th>
-            <th colSpan={MAX_ABILITY_RANK} className={styles.rankCost}>
-              Cost for Rank 1-{MAX_ABILITY_RANK}
+            <th colSpan={rankCount} className={styles.rankCost}>
+              Cost for Rank {startingRank}-{MAX_ABILITY_RANK}
             </th>
-            <th colSpan={MAX_ABILITY_RANK} className={styles.totalCost}>
-              Total to Rank 1-{MAX_ABILITY_RANK}
+            <th colSpan={rankCount} className={styles.totalCost}>
+              Total to Rank {startingRank}-{MAX_ABILITY_RANK}
             </th>
           </tr>
         </thead>
@@ -55,16 +68,20 @@ export class OrbCostsTooltip extends React.Component<Props> {
                 )}
                 {id && itemsById[id].name}
               </td>
-              {(orbCosts[cost] || []).map((n, j) => (
-                <td key={j} className={styles.rankCost}>
-                  {n}
-                </td>
-              ))}
-              {runningTotal(orbCosts[cost] || []).map((n, j) => (
-                <td key={j} className={styles.totalCost}>
-                  {n}
-                </td>
-              ))}
+              {(orbCosts[cost] || []).map((n, j) =>
+                j + 1 < startingRank ? null : (
+                  <td key={j} className={styles.rankCost}>
+                    {n}
+                  </td>
+                ),
+              )}
+              {runningTotal(orbCosts[cost] || []).map((n, j) =>
+                j + 1 < startingRank ? null : (
+                  <td key={j} className={styles.totalCost}>
+                    {n}
+                  </td>
+                ),
+              )}
             </tr>
           ))}
         </tbody>
